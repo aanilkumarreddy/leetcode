046f928de3ffe047afda9d1c8e6729d7500c33b8
Anil Kumar Reddy Allikepalli <45353569+aanilkumarreddy@users.noreply.github.com>
2023-01-08 15:46:48 -0500

    Time: 63 ms (97.63%), Space: 42.8 MB (37.84%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let hash={};
+    // nums.forEach((ele,i)=>{
+    for(let i=0;i<nums.length;i++){
+  if(hash[target-nums[i]]!==undefined){
+        return [hash[target-nums[i]],i]
+    }
+    hash[nums[i]]=i;  
+    }
+    
+// })
+    return []
+};
