204fbba6f6393d2e5e8d3bc0678830402f3cc591
Anil Kumar Reddy Allikepalli <45353569+aanilkumarreddy@users.noreply.github.com>
2023-02-09 00:39:52 -0500

    Time: 52 ms (96.85%), Space: 42 MB (50.29%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -5,10 +5,8 @@
 var subtractProductAndSum = function(n) {
     let sum=0;
     let prod= 1;
-    
     while(n>0){
         let digit = n%10;
-            console.log(digit);
         sum=sum+digit;
         prod= prod * digit;
         n=Math.floor(n/10);
