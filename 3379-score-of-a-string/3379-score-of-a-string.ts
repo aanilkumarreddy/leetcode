cf467ac7f89ea1adbf0b6511bd9f07e7a102b1a0
Anil Kumar Reddy Allikepalli <45353569+aanilkumarreddy@users.noreply.github.com>
2024-11-17 12:59:57 -0500

    Time: 0 ms (100%), Space: 52.3 MB (54.01%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.ts
@@ -0,0 +1,7 @@
+function scoreOfString(s: string): number {
+    let res=0
+    for (let i=0;i<s.length-1;i++){
+        res= res+ Math.abs(s[i].charCodeAt(0)-s[i+1].charCodeAt(0))
+    }
+    return res;
+};
