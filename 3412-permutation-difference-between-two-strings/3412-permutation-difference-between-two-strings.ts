1ffee2ec85f88e36ae27920db7bdc8a3ffba3055
Anil Kumar Reddy Allikepalli <45353569+aanilkumarreddy@users.noreply.github.com>
2024-11-18 19:26:47 -0500

    Time: 0 ms (100%), Space: 52.6 MB (31.52%) - LeetHub

--- 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.ts
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.ts
@@ -1,9 +1,8 @@
 function findPermutationDifference(s: string, t: string): number {
     
     let res=0;
-   const bArr=t.split('');
      for (let i = 0; i < s.length; i++) {
-        res= res+ Math.abs(i-bArr.findIndex(ele=>ele===s.charAt(i)))
+        res= res+ Math.abs(i-t.indexOf(s.charAt(i)))
      }
      return res;
 };
